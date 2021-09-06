f520047ca5f3e7f2a1302c3eab65d1d7bea1c4f3
Guilherme-Jesus <guicoutoo@outlook.com>
2021-09-06 19:04:47 -0300

    Tentando implementar design

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,42 @@
 import React from "react"
-
+import {BoxText, ContainerBox, ContainerCard, ContainerFeed, ContainerFiltro, ContainerFooter, ContainerImg, ContainerTaxa,ImageContainer, ItemFooterCart, ItemFooterHome, ItemFooterUser,TextFour, TextOne, TextRestaurante, TextThree, TextTwo, TimeContainer } from "./styledHomePage"
 
 export const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <ContainerFeed>
+            <h1>HomePage</h1>
+
+            <ContainerFiltro>
+                <TextOne><span>Burger</span></TextOne>
+                <TextTwo><span>Asiatica</span></TextTwo>
+                <TextThree><span>Massas</span></TextThree>
+                <TextFour><span>Saudaveis</span></TextFour>
+            </ContainerFiltro> 
+
+            <ContainerCard>
+
+                  <ContainerBox> 
+
+                <ContainerImg>
+                <ImageContainer src='https://img.itdg.com.br/tdg/images/blog/uploads/2017/07/shutterstock_337714676.jpg?w=1200' alt='foto do sanduba' />
+                  </ContainerImg>
+                   <TextRestaurante>Nome do Restaurante</TextRestaurante>
+                     <TimeContainer> 50 - 60 min </TimeContainer>
+                  <ContainerTaxa> Frete R$6,00  </ContainerTaxa>
+                  </ContainerBox> 
+                
+            </ContainerCard> 
+            
+
+            <ContainerFooter>
+
+            <ItemFooterHome>icon1</ItemFooterHome>
+
+            <ItemFooterCart>icon2</ItemFooterCart>
+
+            <ItemFooterUser>icon3</ItemFooterUser>
+         </ContainerFooter>
+        </ContainerFeed>
     )
 }
 
---
+++ src/pages/HomePage/styledHomePage.js
@@ -0,0 +1,179 @@
+import styled from "styled-components";
+
+
+export const ContainerFeed = styled.div`
+    width: 22.5rem;
+    height: 40rem;
+
+`
+
+export const ContainerFiltro = styled.div`
+ width: 22.5rem;
+  height: 2.625rem;
+  margin: 0.5rem 0 0;
+  padding: 0.75rem 0 0.75rem 1rem;
+`
+export const TextOne = styled.span`
+width: 5.438rem;
+  height: 1.125rem;
+  margin: 0 0.5rem 0 0;
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  color: #e8222e;
+
+
+`
+export const TextTwo = styled.span`
+
+    width: 5.438rem;
+    height: 1.125rem;
+    margin: 0 0.5rem;
+    font-family: Roboto;
+    font-size: 1rem;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    text-align: center;
+    color: var(--black);
+`
+export const TextThree = styled.span`
+  width: 5.438rem;
+  height: 1.125rem;
+  margin: 0 0.5rem;
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  color: var(--black);
+
+`
+export const TextFour = styled.span`
+  width: 3.688rem;
+  height: 1.125rem;
+  margin: 0 0 0 0.5rem;
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  color: var(--black);
+
+`
+export const ContainerCard = styled.div`
+  width: 22.5rem;
+  height: 12.25rem;
+  margin: 3.125rem 0 0;
+  padding: 0.5rem 1rem 0;
+`
+
+
+export const ContainerBox = styled.div`
+  width: 20.5rem;
+  height: 11.75rem;
+  padding: 0 0 1rem;
+  border-radius: 8px;
+  border: solid grey 1px ;
+
+  `
+export const TextRestaurante = styled.span`
+     margin-top: 0px;
+    margin-left: 0px;
+    margin-right: 35px;
+    border-right-width: 22px;
+    padding-right: 140px;
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: #e8222e;
+
+
+`
+export const ImageContainer = styled.img`
+   width: 20.5rem;
+  height: 7.5rem;
+  margin: 0 0 0.75rem;
+  object-fit: contain;
+`
+export const ContainerImg = styled.div`
+  width: 20.5rem;
+  height: 7.5rem;
+  margin: 0 0 0.75rem;
+  border-radius: 8px;
+  background-color: #d8d8d8;
+`
+
+export const TimeContainer = styled.span`
+  width: 9.25rem;
+  height: 1.125rem;
+  margin: 0.25rem 0.5rem 0 1rem;
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: grey;
+
+
+`
+export const ContainerTaxa = styled.span`
+ width: 8.75rem;
+  height: 1.125rem;
+  margin: 0.25rem 1rem 0 0.5rem;
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: right;
+  color: grey;
+`
+export const ContainerFooter= styled.div`
+   width: 22.5rem;
+  height: 3.063rem;
+  margin: 0.813rem 0 0;
+  box-shadow: 0 -1px 3px 0 rgba(0, 0, 0, 0.2), 0 -2px 1px -1px rgba(0, 0, 0, 0.12), 0 -1px 1px 0 rgba(0, 0, 0, 0.14);
+  background-color: #fff;
+`
+
+export const ItemFooterHome = styled.div`
+  width: 7.5rem;
+    height: 3.062rem;
+    padding: 0.688rem 2.875rem 0.688rem 2.938rem;
+`
+
+export const ItemFooterCart = styled.div`
+width: 7.5rem;
+    height: 3.062rem;
+    padding: 0.625rem 2.875rem 0.625rem 2.938rem;
+`
+
+export const ItemFooterUser = styled.div`
+    width: 7.5rem;
+    height: 3.062rem;
+    padding: 0.625rem 2.875rem 0.563rem 2.938rem;
+`
+
+
