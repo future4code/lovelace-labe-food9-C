8b8226acb51808b972277932488ef5cc5dbb84e9
Your Name <you@example.com>
2021-09-08 15:53:09 -0300

    busca de restaurantes

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,15 +1,11 @@
 import React from "react"
-
+import { AllItens, BoxText, ContainerBox, ContainerCard, ContainerFeed, ContainerFiltro, ContainerFooter, ContainerImg, ContainerTaxa, IconeCart, IconeHome, IconeUser, ImageContainer, ItemFooterCart, ItemFooterHome, ItemFooterUser, TextFour, TextOne, TextRestaurante, TextThree, TextTwo, TimeContainer } from "./styledHomePage"
 import InProgressCard from "../../components/InProgressComponent/InProgressCard"
 import ProductQuantity from "../../components/ProductCard/ProductQuantity/ProductQuantity"
 
 
-export const HomePage = () => {
-    return (
-        <div>HomePage
-        </div>
 
-import { AllItens, BoxText, ContainerBox, ContainerCard, ContainerFeed, ContainerFiltro, ContainerFooter, ContainerImg, ContainerTaxa, IconeCart, IconeHome, IconeUser, ImageContainer, ItemFooterCart, ItemFooterHome, ItemFooterUser, TextFour, TextOne, TextRestaurante, TextThree, TextTwo, TimeContainer } from "./styledHomePage"
+
 
 export const HomePage = () => {
     return (
--- src/pages/SearchRestaurantPage/SearchRestaurantPage.js
+++ src/pages/SearchRestaurantPage/SearchRestaurantPage.js
@@ -1,10 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
+import {HeaderContainer} from "../OrderHistoryPage/styledOrderHistory"
+import back from "../../assets/back.svg"
+import axios from "axios"
+import useRequestData from "../../hooks/useRequestData"
+
 
 
 export const SearchRestaurantPage = () => {
+
+    const Restaurant = useRequestData([],`${BASE_URL}/restaurants`)
+  
+
+    const recipeRestaurants = Restaurant.map((restaurant)=>{
+
+   
+    })
     return (
-        <div>SearchRestaurantPage</div>
+
+        <div>
+          
+
+            <HeaderContainer>
+              <p>Restaurante</p>
+              <img src={back} alt={"Ícone de voltar"} />
+                </HeaderContainer>                 
+            <input
+                type="text"
+                value=""
+                onChange=""
+            
+            >Buscar</input>
+            <recipeRestaurants
+            id={""}
+            
+            />
+
+            
+
+            </div>
     )
 }
+
 
 export default SearchRestaurantPage
