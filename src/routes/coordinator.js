5ff87d3d3539f3d8a7fd30f654d7fe4b547a1ae4
Your Name <you@example.com>
2021-09-09 09:58:45 -0300

    ok

---
+++ src/routes/coordinator.js
@@ -0,0 +1,25 @@
+export const goToLogin = (history) => {
+    history.push("/")
+
+}
+
+export const goToSignUp = (history) => {
+    history.push("/cadastrar")
+
+}
+
+export const goBack = (history) => {
+    history.goBack()
+
+}
+
+
+export const goToHome = (history) => {
+    history.push("/home")
+
+}
+
+export const goToRegister = (history) => {
+    history.push("/endereco")
+
+}
